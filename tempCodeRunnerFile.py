c0ceb85824b9fd81dfc48b9ed84930344ec23a5e
hrishavraj89 <hrishabraj89@gmail.com>
2025-07-22 19:11:45 +0530

    Final commit

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -52,3 +52,39 @@ def processCommand(c):
         #Let gemini handle the request!
         speak("Let me think...")
         output = aiProcess(c)
+        speak(output)
+
+
+if __name__ == "__main__":
+    speak("Initializing Jarvis..!")
+    while True:
+        #Listen for the wake word "Jarvis"
+        #obtain audio from the microphone
+        r = sr.Recognizer() 
+        print("Recognizing...")
+
+        # recognize speech using Google
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            
+            #Catch the wake command "jarvis"
+            if(word.lower() == "jarvis"):
+                speak("Yes")
+
+                #Listen for command
+                with sr.Microphone() as source:
+                    print("Jarvis Active... ")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    processCommand(command)
+
+        except Exception as e:
+            print("Error; {0}".format(e))
+
+
+
+
